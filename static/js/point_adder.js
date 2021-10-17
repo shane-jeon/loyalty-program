38f4e22c0dcf032bc07afae26780a3481a5e5ec3
codingtokki <88866019+codingtokki@users.noreply.github.com>
2021-10-17 07:15:38 +0000

    added point counter with JS, have yet to connect to db

--- static/js/point_adder.js
+++ static/js/point_adder.js
@@ -1,2 +1,43 @@
 'use strict';
 
+
+const addTransaction = (serviceAdded) => {
+    $('#add-to-points').append(`
+        <tr>
+            <td>${serviceAdded}</td>)
+        </tr>
+    `);
+};
+
+const resetCount = () => {
+    $('#reward-total-counter').html('0');
+    $('#total-rewards').empty();
+};
+
+const incrementRewardTotal = (cost) => {
+    const rewardTotal = $('#reward-total-counter');
+    
+    let total = Number(rewardTotal.html());
+    total += cost;
+
+    rewardTotal.html(total.toFixed(2));
+};
+
+// const incrementRewardsGained = (amountGained) => {
+//     let rewardGained = Number($('#reward-total-counter').html());
+//     rewardGained += amountGained;
+
+//     $('#reward-total-counter').html(rewardGained);
+// };
+
+$('.add-to-points').on('click', () => {
+    addTransaction('facial');
+    incrementRewardTotal(1);
+});
+
+$('#place-order').on('click', () => {
+    
+    resetCount();
+
+    setProgressAndStatus(0, 'Reward has been cashed in!');
+});
