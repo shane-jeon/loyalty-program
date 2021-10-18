a2a3b384db48cb71c51927c6cee94c3b476843ad
codingtokki <88866019+codingtokki@users.noreply.github.com>
2021-10-18 04:59:03 +0000

    saving w/e I have before I dive into JS and React

---
+++ static/js/pseudocode_for_now.js
@@ -0,0 +1,6 @@
+// user will add one button from one click OR there is also an option to type in the num and add
+// save button afterwards to officially alter a client's reward
+// point adjustment will affect the number of points available in client's account
+// whenever client account point amount reaches 10, the count drops back down to zerio
+
+// hello world
